7a36708d084f3b2625ff38d625d3a674ae3dde5b
mike092 <mr@gmail.com>
2015-08-23 23:57:29 +0200

    Add async file upload

--- Promocje Web/App_Start/BundleConfig.cs
+++ Promocje Web/App_Start/BundleConfig.cs
@@ -9,6 +9,8 @@ namespace Promocje_Web
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryUi").Include(
+                        "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -40,7 +42,7 @@ namespace Promocje_Web
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
---
+++ Promocje Web/Controllers/Api/TempFileController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Promocje_Web.Controllers.Api
+{
+    public class TempFileController : ApiController
+    {
+        // GET: api/TempFile
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/TempFile/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TempFile
+        public void Post([FromBody]string value)
+        {
+            HttpRequestMessage request = this.Request;
+            if (!request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            string root = System.Web.HttpContext.Current.Server.MapPath("~/App_Data/uploads");
+            var provider = new MultipartFormDataStreamProvider(root);
+
+            var task = request.Content.ReadAsMultipartAsync(provider).
+                ContinueWith<HttpResponseMessage>(o =>
+                {
+
+                    string file1 = provider.BodyPartFileNames.First().Value;
+                    // this is the file name on the server where the file was saved 
+
+                    return new HttpResponseMessage()
+                    {
+                        Content = new StringContent("File uploaded.")
+                    };
+                }
+            ); 
+        }
+
+        // PUT: api/TempFile/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TempFile/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
